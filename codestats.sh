330d2eae2062b73369dcb836d36dff6846eb8b3a
Ayman Amhijane <amhijane.dev@gmail.com>
2026-01-12 23:26:03 +0100

    ADDING FILES/FUNCTIONS: Create a codestats.sh script that counts and displays the total lines of C code written

--- codestats.sh
+++ codestats.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# If you enabled the commented line below, this script will list the total number of lines of C code by checking all subdirectories (Entire Project).
+# CODE_LINES=$(git ls-files | grep '.c' | xargs wc -l | grep total | awk '{print $1}')
+
+# This will check only in the current directory.
 CODE_LINES=$(git ls-files | grep '.c'| sed -r 's/libft\/.*//g' | tr -s '' | xargs wc -l | grep total | awk '{print $1}')
 echo "================================================================="
 echo "You write $CODE_LINES lines of C code on this project. Good work!"
